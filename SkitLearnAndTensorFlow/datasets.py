76fe61a850821a7cd544d1cc5c49ea7ec43da3d5
dsalles <dsalles@corp.globo.com>
2020-04-06 22:12:38 -0300

    homf: commit initial

---
+++ SkitLearnAndTensorFlow/datasets.py
@@ -0,0 +1,45 @@
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+import os
+import tarfile
+from six.moves import urllib
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    os.makedirs(housing_path, exist_ok=True)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    fetch_housing_data()
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+
+housing = load_housing_data()
+print(housing.head())
+housing.info()
+housing["ocean_proximity"].value_counts()
+
+
+def split_train_test_by_id(data, test_ratio, id_column, hash=hashlib.md5):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio, hash))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+
+train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "id")
+
+# The  same as this method
+
+
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
